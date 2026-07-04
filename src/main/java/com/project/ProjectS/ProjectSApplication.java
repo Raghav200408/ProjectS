5e45ee700e632484ce42d39a11f05287d828c488
Raghav200408 <narsapuramragava@gmail.com>
2026-07-04 13:36:27 +0530

    Initial project commit

---
+++ src/main/java/com/project/ProjectS/ProjectSApplication.java
@@ -0,0 +1,13 @@
+package com.project.ProjectS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ProjectS/ProjectSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ProjectS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
